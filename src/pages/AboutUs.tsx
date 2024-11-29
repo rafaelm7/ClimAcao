39455f0a94b6976dfb6c7fe7936a9c9a6a7d3951
rafaelm7 <151577207+rafaelm7@users.noreply.github.com>
2024-11-28 22:59:28 -0300

    Update AboutUs.tsx

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -6,7 +6,7 @@ const AboutUs: React.FC = () => {
      <h2 className="text-3xl font-bold mb-6 text-center text-green-600">Sobre Nós</h2>
       <div className="bg-white p-6 rounded-lg shadow-md">
         <p className="mb-4">
-          O ClimAção é um projeto desenvolvido como parte da avaliação A3 da Unidade Curricular de Usabilidade, Desenvolvimento Web, Mobile e Jogos, do curso de Sistemas de Informação e Ciência da Computação. Esse trabalho foi elaborado com o objetivo de aplicar conhecimentos teóricos e práticos sobre usabilidade e desenvolvimento de aplicações digitais, visando criar uma solução que combine funcionalidade, acessibilidade e impacto social.
+          ClimAção é um projeto desenvolvido como parte da avaliação A3 da Unidade Curricular de Usabilidade, Desenvolvimento Web, Mobile e Jogos, do curso de Sistemas de Informação e Ciência da Computação. Esse trabalho foi elaborado com o objetivo de aplicar conhecimentos teóricos e práticos sobre usabilidade e desenvolvimento de aplicações digitais, visando criar uma solução que combine funcionalidade, acessibilidade e impacto social.
         </p>
         <p className="mb-4">
           Orientado pela professora Eliane Isadora Faveron Maciel, o projeto é fruto da colaboração de um grupo de alunos comprometidos em explorar o potencial de tecnologias web e mobile para conscientização sobre mudanças climáticas. Por meio do ClimAção, buscamos promover a educação e o engajamento da comunidade em ações que contribuam para um futuro mais sustentável.
