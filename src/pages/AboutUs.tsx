ad7bafe18434e91384a831f4fff1be67622d94eb
rafaelm7 <151577207+rafaelm7@users.noreply.github.com>
2024-11-28 22:52:46 -0300

    Update AboutUs.tsx

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -6,20 +6,22 @@ const AboutUs: React.FC = () => {
      <h2 className="text-3xl font-bold mb-6 text-center text-green-600">Sobre Nós</h2>
       <div className="bg-white p-6 rounded-lg shadow-md">
         <p className="mb-4">
-          A ClimAção é uma organização dedicada a promover a conscientização ambiental e ações sustentáveis para combater as mudanças climáticas. Nossa missão é inspirar e capacitar indivíduos e comunidades a fazerem escolhas mais ecológicas em seu dia a dia.
+          O ClimAção é um projeto desenvolvido como parte da avaliação A3 da Unidade Curricular de Usabilidade, Desenvolvimento Web, Mobile e Jogos, do curso de Sistemas de Informação e Ciência da Computação. Esse trabalho foi elaborado com o objetivo de aplicar conhecimentos teóricos e práticos sobre usabilidade e desenvolvimento de aplicações digitais, visando criar uma solução que combine funcionalidade, acessibilidade e impacto social.
         </p>
         <p className="mb-4">
-          Trabalhamos para educar, motivar e facilitar mudanças positivas em prol do nosso planeta.
+          Orientado pela professora Eliane Isadora Faveron Maciel, o projeto é fruto da colaboração de um grupo de alunos comprometidos em explorar o potencial de tecnologias web e mobile para conscientização sobre mudanças climáticas. Por meio do ClimAção, buscamos promover a educação e o engajamento da comunidade em ações que contribuam para um futuro mais sustentável.
         </p>
-        <h2 className="text-2xl font-semibold mb-3">Nossos Objetivos:</h2>
+        <h2 className="text-2xl font-semibold mb-3">Equipe de Desenvolvimento:</h2>
         <ul className="list-disc pl-6 mb-4">
-          <li>Educar o público sobre as causas e impactos das mudanças climáticas</li>
-          <li>Promover práticas sustentáveis em comunidades e empresas</li>
-          <li>Desenvolver e implementar soluções inovadoras para reduzir a pegada de carbono</li>
-          <li>Colaborar com organizações locais e globais para amplificar nosso impacto positivo</li>
+          <li>Ian Freire Borges - 12723134698</li>
+          <li>Raí Lapa de Souza - 12723132815</li>
+          <li>Irvin Marques de Moura - 12723123617</li>
+          <li>Rafael Cordeiro Magalhães - 1272019188</li>
+          <li>Rafael Teixeira Queiroz de Carvalho - 12723129558</li>
+          <li>Pedro Henrique Martins Mattos Barreiros - 1272319265</li>
         </ul>
         <p>
-          Junte-se a nós nesta jornada para criar um futuro mais verde e sustentável. Cada ação conta, e juntos podemos fazer a diferença!
+          Com dedicação e inovação, esperamos que o ClimAção inspire novas iniciativas e contribua para um impacto positivo no mundo.
         </p>
       </div>
     </div>
